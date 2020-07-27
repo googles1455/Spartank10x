5c4885c6683eed163b600addf4751478bcdd52de
googles1455 <cool28991@gmail.com>
2020-07-27 08:19:04 -0700

    first commit

---
+++ Power.java
@@ -0,0 +1,14 @@
+public class Power{
+public static void main(String []args)
+{
+int n=16;
+if(n%2==0)
+{
+System.out.println("power of two");
+}
+else
+{
+System.out.println("not a power of two");
+}
+}
+}
